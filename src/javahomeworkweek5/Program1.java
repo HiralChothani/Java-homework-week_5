6a5380611d656bd1494818a5522506628f953c86
HiralChothani <hiralchothani5@gmail.com>
2023-03-06 19:59:13 +0000

    my homework week 5

---
+++ src/javahomeworkweek5/Program1.java
@@ -0,0 +1,27 @@
+package javahomeworkweek5;
+
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("            BIO DATA   ");
+        System.out.print("Name            :"  );
+        System.out.println("          Radha Kishan  ");
+        System.out.println("Date Of Birth   :          21/08/1453");
+        System.out.println("Place Of Birth  :          Barsana ");
+        System.out.println("                           Uttar Pradesh");
+        System.out.println("                           India");
+        System.out.println("Height                     5 ft 9 in");
+        System.out.println("Religion        :          Hindu");
+        System.out.println("           Family Detail     " );
+        System.out.println("Father's Name   :          Vrishabhanu   ");
+        System.out.println("Mother's Name   :          Kirtida");
+        System.out.println("Brother's Name  :          Sridama");
+        System.out.println("Sister's Name   :          Ananga-manjari");
+        System.out.println("           Contact Details   ");
+        System.out.println("Contact Number  :          9999999999");
+
+
+
+    }
+
+
+}
